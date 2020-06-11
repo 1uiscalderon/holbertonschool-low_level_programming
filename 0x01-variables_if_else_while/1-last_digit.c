fcb09e986e8f2466156be515c404591ed8d32c00
1uiscalderon <1857@holbertonschool.com>
2020-06-11 21:42:22 +0000

    Prints if the last number is + - or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ulti);
 	else if (ulti == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ulti);
-	else (ulti < 6 && != 0)
-	   	printf("Last digit of %d is %d and is 0\n", n, ulti);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ulti);
 	return (0);
 }

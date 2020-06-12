fc523302e166d4c48c074fccdf13e1af28a37f61
1uiscalderon <1857@holbertonschool.com>
2020-06-12 04:09:59 +0000

    3 Digits to find posible convinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of numbers without repeating using putchar
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 49;
+	int k = 50;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+			k = j + 1;
+		}
+		i++;
+		j = i;
+	}
+	putchar('\n');
+	return (0);
+}

3eba0e3b25ee922991443ba43c4d39cc10885a6a
1uiscalderon <1857@holbertonschool.com>
2020-06-15 22:36:52 +0000

    Getting started

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	int i;
-	char n[9] = 'Holberton';
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar (n[i]);
-	}
+	_putchar ('H');
+	_putchar ('o');
+	_putchar ('l');
+	_putchar ('b');
+	_putchar ('e');
+	_putchar ('r');
+	_putchar ('t');
+	_putchar ('o');
+	_putchar ('n');
 	_putchar ('\n');
 	return (0);
 }

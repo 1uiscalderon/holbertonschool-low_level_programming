9382bb335158a3157efcdd716a645947673ec1bb
1uiscalderon <1857@holbertonschool.com>
2020-06-16 16:27:21 +0000

    Prints the absolute valor o an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _abs - prints the absolute valor of a number
+ *@n: is a parameter
+ * Return: n if n is positive, -n if n is negative
+ */
+int _abs(int n);
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}

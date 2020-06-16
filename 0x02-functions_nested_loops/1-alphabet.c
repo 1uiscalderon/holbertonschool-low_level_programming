90d8d64af8d159cccfa74ae5978a1ae419b5f525
1uiscalderon <1857@holbertonschool.com>
2020-06-16 01:28:04 +0000

    Print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - prints the alphabet using putchar
+ * Return: 0 if success
+ */
+void print_alphabet(void)
+{
+	char n;
+	n = 'a';
+
+	while (n <= 'z')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}

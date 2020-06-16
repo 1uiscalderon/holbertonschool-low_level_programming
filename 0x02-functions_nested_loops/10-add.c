86a1eb626db1bac325fb7331996c4b12810c1e5d
1uiscalderon <1857@holbertonschool.com>
2020-06-16 22:03:25 +0000

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - returns 1 if a letter is lower or upper case
+ *@c: is a parameter
+ * Return: 1 if c is lowercase, 0 if it isnt
+ */
+int add(int, int)
+{
+	int sum;
+
+	sum = a + b;
+	return (c);
+}

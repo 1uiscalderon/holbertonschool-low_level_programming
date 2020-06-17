a2f0b805b55942f15620e53be49a2a43e0e988d7
1uiscalderon <1857@holbertonschool.com>
2020-06-17 22:28:27 +0000

    First 50 numbers of fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - compute and prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: 0 is success
+ */
+int main(void)
+{
+	long int i, j, k, n;
+	i = 1;
+	j = 2;
+
+	for (n = 0; n < 50; n++)
+	{
+		if (n < 49)
+		{
+			printf("%ld, ", i);
+			k = j  + i;
+			i = j;
+			j = k;
+		}
+		else
+		{
+			printf("%ld\n", i);
+		}
+	}
+		return (0);
+}

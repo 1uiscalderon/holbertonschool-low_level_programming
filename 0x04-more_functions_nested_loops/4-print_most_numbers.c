7766c5ff2ef767e66086c99def73a96ed3d3277a
1uiscalderon <1857@holbertonschool.com>
2020-06-18 20:47:55 +0000

    Print most of the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints the most of numbers from 0 to 9 using putchar
+ *
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	n = '0';
+
+	while (n <= '9' && )
+	{
+		if (n != '2' && n != '4')
+		{
+		_putchar(n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}

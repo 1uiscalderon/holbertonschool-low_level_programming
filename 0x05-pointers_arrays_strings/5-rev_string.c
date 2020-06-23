1a23947292774f86a6c7aee9faad8800de4a558b
1uiscalderon <1857@holbertonschool.com>
2020-06-23 22:17:04 +0000

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_rev - Prints a string, in reverse, followed by a new line
+ *@s: is a parameter
+ */
+void print_rev(char *s)
+{
+	char r;
+	int i, j, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	c = i - 1;
+	for (j = 0; j <= c; j++)
+	{
+		r = s[j];
+		s[j] = s[i];
+		s[i] = r;
+		i--;
+	}
+}

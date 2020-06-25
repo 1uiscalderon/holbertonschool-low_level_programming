af49de69cda6d81e09c40e488add98b47109e0c0
1uiscalderon <1857@holbertonschool.com>
2020-06-25 03:03:20 +0000

    Converts a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *_atoi - Converts a string to an integer
+ *@s: is a parameter
+ */
+int _atoi(char *s)
+{
+	int i, res, num, signo = 1;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			signo = -signo;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = s[i];
+			num = num - '0';
+			res = res + (num * 10);
+		}
+		else
+		{
+			break;
+		}
+	}
+	res = res * signo;
+	return(res)
+}

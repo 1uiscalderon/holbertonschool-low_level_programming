210469603cfd95a98445fcfd95fbadd23ff739d2
1uiscalderon <1857@holbertonschool.com>
2020-06-26 03:57:46 +0000

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *cap_string - Capitalizes a string
+ *@s: is a parameter
+ *Return: Capitalized string s
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	char sepa[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
+		       '{', '}'};
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		for (j = 0; sepa[j] != '\0'; j++)
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}

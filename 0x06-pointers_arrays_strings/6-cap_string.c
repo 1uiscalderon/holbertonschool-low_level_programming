21cf1d2707abfda4cfbcf226a3c84b8de4d25a20
1uiscalderon <1857@holbertonschool.com>
2020-06-26 04:06:31 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,13 +13,13 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0' ; i++)
 	{
-		if (s[i] == 0 && s[i] >= 'a' && s[i] <= 'z')
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
 		}
 		for (j = 0; sepa[j] != '\0'; j++)
 		{
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z' && s[i] == sepa[j])
 			{
 				s[i + 1] = s[i + 1] - 32;
 			}

66a28684e490a53c7b93cd7c17124f5f54694ef9
1uiscalderon <1857@holbertonschool.com>
2020-06-29 15:43:07 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - fills memory with a constant byte
+ *@s: is a parameter
+ *@c: is a parameter
+ *Return: Return the value of s if is the same as c, else NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+	}
+
+	return (NULL);
+}

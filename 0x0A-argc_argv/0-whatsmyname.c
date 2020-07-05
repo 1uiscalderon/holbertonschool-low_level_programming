2e1265c720ddbfefb80369f09f8d527fb91badd6
1uiscalderon <1857@holbertonschool.com>
2020-07-05 19:17:00 +0000

    Print Programs name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints the programs name
+ * @argc: Number of arguments in program
+ * @argv: Argument vectors
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

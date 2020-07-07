e051fc3b82c26fa50eb7f008f122156b9b9b50f4
1uiscalderon <1857@holbertonschool.com>
2020-07-07 20:32:21 +0000

    Copy a string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory
+ *           with the copied string
+ * @str: String
+ *Return: Pointer of the copied string
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *dest;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	dest = (char *)malloc(len * sizeof(char));
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = str[i];
+	}
+
+	return (dest);
+}

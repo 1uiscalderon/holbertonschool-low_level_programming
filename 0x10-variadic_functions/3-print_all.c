82200d79924114691b1bcb0c66d43878906f313b
1uiscalderon <1857@holbertonschool.com>
2020-07-20 04:09:42 +0000

    Fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -18,25 +18,26 @@ void print_all(const char *const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(argum, int));
-			flag = 1, break;
+			printf("%c", va_arg(argum, int)), flag = 1;
+			break;
 		case 'i':
-			printf("%d", va_arg(argum, int));
-			flag = 1, break;
+			printf("%d", va_arg(argum, int)), flag = 1;
+			break;
 		case 'f':
-			printf("%f", va_arg(argum, double));
-			flag = 1, break;
+			printf("%f", va_arg(argum, double)), flag = 1;
+			break;
 		case 's':
 			string = va_arg(argum, char *);
 			if (string == NULL)
 			{
-				printf("(nil)");
-				flag = 1, break;
+				printf("(nil)"), flag = 1;
+				break;
 			}
-			printf("%s", string);
-			flag = 1, break;
+			printf("%s", string), flag = 1;
+			break;
 		default:
-			flag = 0, break;
+			flag = 0;
+			break;
 		}
 		if (flag == 1 && format[i + 1] != '\0')
 			printf(", ");

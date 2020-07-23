bfbc86f6f80e3127b27a94653c97d09ec58251c1
1uiscalderon <1857@holbertonschool.com>
2020-07-23 22:22:45 +0000

    returns the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list
+ * list_len - Returns the number of elements in a linked list_t list
  * @h: Pointer to the node to print
- * Return: Number of nodes in the list
+ * Return: Number number of elements
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int i;
 

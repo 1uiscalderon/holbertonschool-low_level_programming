85ce812f957e501694a43b624eabf117c5399d75
1uiscalderon <1857@holbertonschool.com>
2020-07-28 17:03:44 +0000

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (tmp == NULL)
 		return (NULL);
 	new->next = tmp->next;
-	temp->next = new;
+	tmp->next = new;
 	return (new);
 }

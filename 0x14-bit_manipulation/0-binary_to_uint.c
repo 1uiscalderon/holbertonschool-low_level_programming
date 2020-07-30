244e8777d68d5179c331a9817e1b08c33528fed7
1uiscalderon <1857@holbertonschool.com>
2020-07-30 20:18:03 +0000

    Binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: String with number to convert
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec = 2 * dec + (b[i] - '0');
+	}
+	return (dec);
+}

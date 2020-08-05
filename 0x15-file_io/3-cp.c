8cf1fbf7b45e0b6b93de5ead026cdd361de6363b
1uiscalderon <1857@holbertonschool.com>
2020-08-05 02:25:08 +0000

    Copies a content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_o, file_d, r, w, c;
+	int file_o, file_d, r = 1024, w, c;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -25,19 +25,20 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((r = read(file_o, buffer, 1024)) > 0)
+	while (r == 1024)
 	{
+		r = read(file_o, buffer, 1024);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		w = write(file_d, buffer, r);
-	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (w == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	c = close(file_o);
 	if (c == -1)

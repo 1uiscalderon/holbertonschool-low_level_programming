bc293f735aef1269f7a042eabe0111a22aab4343
1uiscalderon <1857@holbertonschool.com>
2020-09-06 17:44:18 +0000

    Inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		if (i == idx - 1)
 		{
-			if (temp->next == NULL)
+			if (tmp->next == NULL)
 				return (add_dnodeint_end(h, n));
 			new->next = tmp->next;
 			new->prev = tmp->prev;
